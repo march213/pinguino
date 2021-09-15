9b67bf1a57cfaaa2ee857325917250f36606a66e
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-09-15 03:21:13 +0000

    use grid

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-function add(a, b) {
-  return a + b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-let year = 2042;
-let nextYear = year + 1;
-
 let canvas = document.querySelector(".canvas");
 let ctx = canvas.getContext("2d");
 
-for (let i = 1; i <= 5; i++) {
-  let width = 60;
-  let height = 60;
-  let gap = 40;
-  let x = 20 + (width + gap) * i;
-  let y = 20;
+ctx.lineWidth = 2;
 
-  ctx.lineWidth = 2;
-  ctx.beginPath();
-  ctx.rect(x, y, width, height);
-  ctx.stroke();
-  ctx.closePath();
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 5; j++) {
+    let width = 60;
+    let height = 60;
+    let gap = 40;
+    let x = 10 + (width + gap) * i;
+    let y = 10 + (height + gap) * j;
+
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.rect(x + 8, y + 8, width - 16, height - 16);
+    ctx.stroke();
+    ctx.closePath();
+  }
 }
