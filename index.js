3bbb597fb32eb81bf5a796566f048533b53767da
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-09-15 03:24:08 +0000

    add randomness

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 let canvas = document.querySelector(".canvas");
 let ctx = canvas.getContext("2d");
 
-ctx.lineWidth = 2;
+ctx.lineWidth = 3;
 
-for (let i = 1; i <= 5; i++) {
-  for (let j = 1; j <= 5; j++) {
-    let width = 60;
-    let height = 60;
-    let gap = 40;
-    let x = 10 + (width + gap) * i;
-    let y = 10 + (height + gap) * j;
+function drawSquares() {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let width = 60;
+      let height = 60;
+      let gap = 40;
+      let x = 100 + (width + gap) * i;
+      let y = 100 + (height + gap) * j;
 
-    ctx.beginPath();
-    ctx.rect(x, y, width, height);
-    ctx.stroke();
-    ctx.closePath();
+      ctx.beginPath();
+      ctx.rect(x, y, width, height);
+      ctx.stroke();
+      ctx.closePath();
 
-    ctx.beginPath();
-    ctx.rect(x + 8, y + 8, width - 16, height - 16);
-    ctx.stroke();
-    ctx.closePath();
+      // if (i > 0 && i < 4) {
+      //   ctx.beginPath();
+      //   ctx.rect(x + 8, y + 8, width - 16, height - 16);
+      //   ctx.stroke();
+      //   ctx.closePath();
+      // }
+
+      if (Math.random() > 0.5) {
+        ctx.beginPath();
+        ctx.rect(x + 8, y + 8, width - 16, height - 16);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
   }
 }
+
+drawSquares();
