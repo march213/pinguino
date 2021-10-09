3bbaa4dbc41739afc1f0d6af2a17002048e8f7be
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-10-09 13:46:19 -0400

    make animated agents

---
+++ sketches/sketch-03.js
@@ -0,0 +1,90 @@
+const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+
+const settings = {
+  dimensions: [1080, 1080],
+  animate: true,
+};
+
+const sketch = ({ context, width, height }) => {
+  const agents = [];
+
+  for (let i = 0; i < 40; i += 1) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agents.push(new Agent(x, y));
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < agents.length; i += 1) {
+      const agent = agents[i];
+
+      for (let j = 0; j < agents.length; j += 1) {
+        const other = agents[j];
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+        context.closePath();
+      }
+    }
+
+    agents.forEach((agent) => {
+      agent.update();
+      agent.draw(context);
+      agent.bounce(width, height);
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
+  draw(context) {
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+    context.closePath();
+
+    context.restore();
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) {
+      this.vel.x *= -1;
+    }
+
+    if (this.pos.y <= 0 || this.pos.y >= height) {
+      this.vel.y *= -1;
+    }
+  }
+}
