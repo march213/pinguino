72b6661350616963b3eec5f30a2cd23ab3e9b0e5
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-10-12 23:17:16 -0400

    add gui to  tweak params, setup some params

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,19 +1,29 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const math = require("canvas-sketch-util/math");
+const Tweakpane = require('tweakpane')
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
 };
 
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  frequency: 0.001,
+  amplitude: 0.2,
+}
+
 const sketch = () => {
   return ({ context, width, height, frame }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 25;
-    const rows = 25;
+    const cols = params.cols;
+    const rows = params.rows;
     const numOfCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -33,10 +43,10 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x + frame * 10, y, 0.001);
-      const angle = n * Math.PI * 0.2;
+      const n = random.noise2D(x + frame * 10, y, params.frequency);
+      const angle = n * Math.PI * params.amplitude;
       // const scale = (n * 0.5 + 0.5) * 30;
-      const scale = math.mapRange(n, -1, 1, 1, 30);
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       context.save();
       context.translate(x, y);
@@ -57,4 +67,20 @@ const sketch = () => {
   };
 };
 
+const createPane = () => {
+  const pane = new Tweakpane.Pane()
+  let folder
+
+  folder = pane.addFolder({ title: 'Grid' })
+  folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 })
+  folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 })
+  folder.addInput(params, 'scaleMin', { min: 1, max: 100 })
+  folder.addInput(params, 'scaleMax', { min: 1, max: 100 })
+
+  folder = pane.addFolder({ title: 'Noise' })
+  folder.addInput(params, 'frequency', { min: -0.01, max: 0.01 })
+  folder.addInput(params, 'amplitude', { min: 0, max: 1 })
+}
+
+createPane()
 canvasSketch(sketch, settings);
